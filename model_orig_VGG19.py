f99afbc296c76938a39144617545f6827deb462a
sai rohit battula <sai rohit battula>
2018-12-02 18:46:02 -0600

    model almost working

--- model_orig_VGG19.py
+++ model_orig_VGG19.py
@@ -61,6 +61,14 @@ def restore_image(processed_image):
     x = x + means
     return x
 
+def get_style_loss(base_style, gram_target):
+  """Expects two images of dimension h, w, c"""
+  # height, width, num filters of each layer
+  # We scale the loss at a given layer by the size of the feature map and the number of filters
+  height, width, channels = base_style.get_shape().as_list()
+  gram_style = gram_matrix(base_style)
+  
+  return tf.reduce_mean(tf.square(gram_style - gram_target))
 
 def gram_matrix(input_tensor):
 
@@ -71,11 +79,20 @@ def gram_matrix(input_tensor):
     gram = tf.matmul(a, a, transpose_a=True)
     return gram / tf.cast(n, tf.float32)
 
+def get_style_loss(base_style, gram_target):
+  """Expects two images of dimension h, w, c"""
+  # height, width, num filters of each layer
+  # We scale the loss at a given layer by the size of the feature map and the number of filters
+  height, width, channels = base_style.get_shape().as_list()
+  gram_style = gram_matrix(base_style)
+  
+  return tf.reduce_mean(tf.square(gram_style - gram_target))# / (4. * (channels ** 2) * (width * height) ** 2)
+
 
 def run(content_path, style_path, itrNum, content_weight, style_weight):
 
     # get the images
-    tf.enable_eager_execution()
+    # tf.enable_eager_execution()
 
     content_image = load_img_preprocess(content_path)
 
@@ -95,14 +112,14 @@ def run(content_path, style_path, itrNum, content_weight, style_weight):
     style_layers = []
 
     for i in range(1, 5):
-        style_layers.append('block{0}_conv1'.format(i))
+        style_layers.append('block{}_conv1'.format(i))
 
-    content_out_layers = [pretrained_net.get_layer('block5_conv2')]
+    content_out_layers = [pretrained_net.get_layer('block5_conv2').output]
 
     style_out_layers = []
 
     for layer_name in style_layers:
-        style_out_layers.append(pretrained_net.get_layer(layer_name))
+        style_out_layers.append(pretrained_net.get_layer(layer_name).output)
 
     model_out = content_out_layers + style_out_layers
 
@@ -123,6 +140,67 @@ def run(content_path, style_path, itrNum, content_weight, style_weight):
     init_image = tensor_eager.Variable(init_image, dtype=tf.float32)
     # Create our optimizer
     opt = tf.train.AdamOptimizer(learning_rate=5, beta1=0.99, epsilon=1e-1)
+
+    loss_best = float('inf')
+    best_img = None
+
+    # clip the input image 
+    # using max dim and min dim
+    max_dim = -np.array([103.939, 116.779, 123.68])
+    min_dim = 255-np.array([103.939, 116.779, 123.68]) 
+
+    for i in range(itrNum):
+        # compute loss for all layers
+        # compute style loss 
+        # compute content loss
+        # compute their sum and produce gradients over the total loss
+        # optimize using the total loss and the input image
+        with tf.GradientTape() as gradi:
+            out_final_entireImage = main_model(init_image)
+            style_out = out_final_entireImage[:5]
+            content_out = main_model[5:]
+            stylePoints = 0
+            contentPoints = 0
+
+            # equal weight across the contributions of all layers
+            content_layer_norm = 1/float(5)
+            for out, inter in zip(content_features, content_out):
+                contentPoints= contentPoints + content_layer_norm*(tf.reduce_mean(tf.square(inter[0] - out)))
+
+            
+            style_layer_norm = 1/float(5)
+            for out, inter in zip(gram_style_features, style_out):
+                stylePoints = stylePoints + style_layer_norm*get_style_loss(inter[0],out)
+
+            contentPoints = contentPoints*contentPoints
+            stylePoints = stylePoints*stylePoints
+
+            totalLoss = contentPoints + stylePoints
+
+            gradients = gradi.gradient(totalLoss, init_image)
+
+        opt.apply_gradients([(gradients,init_image)])
+
+        clip_initImage = tf.clip_by_value(init_image, min_dim, max_dim)
+
+        init_image.assign(clip_initImage)
+
+        if(totalLoss<loss_best):
+            loss_best = totalLoss
+            best_img = restore_image(init_image)
+
+    return best_img, loss_best
+
+tf.enable_eager_execution()
+print("Eager execution: {}".format(tf.executing_eagerly()))
+
+best_starry_night, best_loss = run('src_images/Tuebingen_Neckarfront.jpg','src_images/1024px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg', 10,1e3,1e-2)
+
+
+
+
+
+
 
 
 
