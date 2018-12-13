f0fb2de0ddc63f2922f2c4e0d93f78eaebd8b91a
Swaraj <srao24@wisc.edu>
2018-12-12 19:29:43 -0600

    Added support for multiple style images, moved images back to root src_images

--- StyleTransfer.py
+++ StyleTransfer.py
@@ -3,11 +3,13 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.eager as tfeager
 from tensorflow.contrib.learn.python.learn import trainable
+from tensorflow.python.keras.preprocessing import image as kp_image
 import tensorflow.contrib.eager as tensor_eager
 from tensorflow.python.keras import models
 from tensorflow.python.keras import losses
 from tensorflow.python.keras import layers
 from tensorflow.python.keras import backend as K
+
 from PIL import Image
 from datetime import datetime
 import matplotlib.pyplot as plt
@@ -24,21 +26,25 @@ OPTIMIZER = 'adam'
 tf.enable_eager_execution()
 
 @Gooey(advanced = True,
-    program_name = "Neural Style Transfer",
-    program_description = "Enter arguments in order to run the Style Transfer program.",
-    show_stop_warning = True,
-    force_stop_is_error = True,
-    show_success_modal = True,
-    run_validators = True,
-    show_sidebar = False,
-    image_dir = 'gooey_image_dir/',
-    progress_regex = r"^iteration: (?P<current>\d+)/(?P<total>\d+)$",
-    progress_expr="current/total * 100")
-def build_parser(): #TODO: add poll_external_updates to use dynamic values
+program_name = "Neural Style Transfer",
+program_description = "Enter arguments in order to run the Style Transfer program.",
+show_stop_warning = True,
+force_stop_is_error = True,
+show_success_modal = True,
+run_validators = True,
+show_sidebar = False,
+image_dir = 'gooey_image_dir/',
+progress_regex = r"^iteration: (?P<current>\d+)/(?P<total>\d+)$",
+progress_expr="current/total * 100")
+def build_parser():
     parser = GooeyParser(description="Choose input parameters for Neural Style Transfer")
     parser.add_argument('content', help='content image',
             metavar='CONTENT', widget = "FileChooser")
     parser.add_argument('style', help='style image',
+            metavar='STYLE', widget = "FileChooser")
+    parser.add_argument('--style2', help='style image',
+            metavar='STYLE', widget = "FileChooser")
+    parser.add_argument('--style3', help='style image',
             metavar='STYLE', widget = "FileChooser")
     parser.add_argument('--iterations', type=int,
             dest='iterations', help='number of iterations to run for',
@@ -64,8 +70,19 @@ def main():
     parser = build_parser()
     args = parser.parse_args()
     content_image = load_img_preprocess(args.content)
-    style_image = load_img_preprocess(args.style)
+    if(args.style2 is None):
+        style_paths = [args.style]
+    elif(args.style3 is None):
+        style_paths=[args.style, args.style2]
+    else:
+        style_paths=[args.style, args.style2, args.style3]
+    style_images = []
 
+    #make an array and add all style images produced from style paths given as input
+    for i in style_paths:
+    #call the load and preprocess function to load the style images from the given path
+        print(i)
+        style_images.append(load_img_preprocess(i))
     """
     get the model from keras basically lets us extract the layers
     and their corresponding intermediate and batch outputs
@@ -95,13 +112,17 @@ def main():
 
     for layer in main_model.layers:
         layer.trainable = False
-
-    style_features = [style_layer[0]
-                      for style_layer in main_model(style_image)[:5]]
+    style_features = []
+    for i in style_images:
+        style_features.append([style_layer[0]
+                        for style_layer in main_model(i)[:5]])
+        
     content_features = [content_layer[0]
                         for content_layer in main_model(content_image)[5:]]
+    gram_style_features = []
 
-    gram_style_features = [gram_matrix(style_feature) for style_feature in style_features]
+    for i in style_features:
+        gram_style_features.append([gram_matrix(style_feature) for style_feature in i])
 
     # Set initial image
     init_image = load_img_preprocess(args.content)
@@ -142,8 +163,9 @@ def main():
 
             
             style_layer_norm = 1/float(5)
-            for out, inter in zip(gram_style_features, style_out):
-                stylePoints = stylePoints + style_layer_norm*get_style_loss(inter[0],out)
+            for grams in gram_style_features:
+                for out, inter in zip(grams, style_out):
+                    stylePoints = stylePoints + style_layer_norm*get_style_loss(inter[0],out)
 
             contentPoints = contentPoints*contentPoints
             stylePoints = stylePoints*stylePoints
@@ -170,35 +192,65 @@ def main():
     plt.imsave(output_name, display_img)
     plt.show()
 
-
+# referece
+# citing URL: https://keras.io/applications/#vgg19
 def load_img_preprocess(image_path):
     img_str = tf.read_file(image_path)
+
     img_decode = tf.image.decode_jpeg(img_str, 3)
+
     img = tf.cast(img_decode, tf.float32)
+
     dim =512.0
+
     height = tf.to_float(tf.shape(img)[1])
+
     width = tf.to_float(tf.shape(img)[0])
-    scale = tf.cond(tf.greater(height, width), lambda: dim/width, lambda: dim/height)
+
+    print('this is the old height and width ', height, width)
+    scale = tf.cond(tf.greater(height, width), lambda: dim/width , lambda: dim/height)
+    print('this is the scale ', scale)
+
     newHeight = tf.to_int32(height * scale)
     newWidth = tf.to_int32(width * scale)
+    print('newheight and new width', newHeight, newWidth)
+
     img = tf.image.resize_images(img, [newHeight, newWidth])
-    img = np.expand_dims(img, axis=0)
-    VGG_MEAN = [123.68, 116.78, 103.94] #RGB Values for ImageNet
+
+    """VGG_MEAN = [123.68, 116.78, 103.94]  # This is R-G-B for Imagenet
+
+    img = tf.random_crop(img, [224, 224, 3])
     means = tf.reshape(tf.constant(VGG_MEAN), [1, 1, 3])
     img = img - means
+    """
+    img = np.expand_dims(img, axis=0)
+
+    VGG_MEAN = [123.68, 116.78, 103.94]
+
+    means = tf.reshape(tf.constant(VGG_MEAN), [1, 1, 3])
+    img = img - means
+    max_dim = 512
+    img = Image.open(image_path)
+    norm = max(img.size)
+    scale = max_dim/norm
+    img = img.resize((round(img.size[0]*scale), round(img.size[1]*scale)), Image.ANTIALIAS)
+    img = kp_image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = tf.keras.applications.vgg19.preprocess_input(img)
+
     return img
 
 
 def deprocess_img(processed_img):
-  x = processed_img
-  # perform the inverse of the preprocessiing step
-  x[:, :, 0] += 103.939
-  x[:, :, 1] += 116.779
-  x[:, :, 2] += 123.68
-  x = x[:, :, ::-1]
+    x = processed_img
+    # perform the inverse of the preprocessiing step
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    x = x[:, :, ::-1]
 
-  x = np.clip(x, 0, 255).astype('uint8')
-  return x
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
 
 
 def restore_image(processed_image):
@@ -211,17 +263,17 @@ def restore_image(processed_image):
     return x
 
 def get_style_loss(base_style, gram_target):
-  """Expects two images of dimension h, w, c"""
-  # height, width, num filters of each layer
-  # We scale the loss at a given layer by the size of the feature map and the number of filters
-  height, width, channels = base_style.get_shape().as_list()
-  gram_style = gram_matrix(base_style)
-  
-  return tf.reduce_mean(tf.square(gram_style - gram_target))
+    """Expects two images of dimension h, w, c"""
+    # height, width, num filters of each layer
+    # We scale the loss at a given layer by the size of the feature map and the number of filters
+    height, width, channels = base_style.get_shape().as_list()
+    gram_style = gram_matrix(base_style)
+
+    return tf.reduce_mean(tf.square(gram_style - gram_target))
 
 def gram_matrix(input_tensor):
 
-    # We make the image channels first
+# We make the image channels first
     channels = int(input_tensor.shape[-1])
     a = tf.reshape(input_tensor, [-1, channels])
     n = tf.shape(a)[0]
